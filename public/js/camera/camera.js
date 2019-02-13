d1de0a34bd7849460d9523e048006374031a593a
Kevin Wylder <wylderkevin@gmail.com>
2019-02-12 19:30:31 -0800

    Add wireframe to one target

--- public/js/camera/camera.js
+++ public/js/camera/camera.js
@@ -18,7 +18,8 @@ function startVideo() {
         });
     }
 
-    window.setInterval(renderFrame, 10000);
+    renderFrame();
+    window.setInterval(renderFrame, 1000);
 }
 
 function renderFrame() {
@@ -36,3 +37,58 @@ function renderFrame() {
     }).catch(console.log);
 }
 
+const connectedPartNames = [ ['leftHip', 'leftShoulder'], ['leftElbow', 'leftShoulder'], ['leftElbow', 'leftWrist'], ['leftHip', 'leftKnee'], ['leftKnee', 'leftAnkle'], ['rightHip', 'rightShoulder'], ['rightElbow', 'rightShoulder'], ['rightElbow', 'rightWrist'], ['rightHip', 'rightKnee'], ['rightKnee', 'rightAnkle'], ['leftShoulder', 'rightShoulder'], ['leftHip', 'rightHip'] ];
+const partNames = [ 'nose', 'leftEye', 'rightEye', 'leftEar', 'rightEar', 'leftShoulder', 'rightShoulder', 'leftElbow', 'rightElbow', 'leftWrist', 'rightWrist', 'leftHip', 'rightHip', 'leftKnee', 'rightKnee', 'leftAnkle', 'rightAnkle' ];
+const partIds = partNames.reduce((result, jointName, i) => { result[jointName] = i; return result; }, {});
+
+const connectedPartIndices = connectedPartNames.map(([jointNameA, jointNameB]) => ([partIds[jointNameA], partIds[jointNameB]]));
+
+function eitherPointDoesntMeetConfidence( a, b, minConfidence) {
+  return (a < minConfidence || b < minConfidence);
+}
+
+function getAdjacentKeyPoints(keypoints, minConfidence) {
+  return connectedPartIndices.reduce(
+      (result, [leftJoint, rightJoint]) => {
+        console.log(keypoints, leftJoint, rightJoint);
+        if (eitherPointDoesntMeetConfidence(
+                keypoints[leftJoint].score, keypoints[rightJoint].score, minConfidence)) {
+          return result;
+        }
+
+        result.push([keypoints[leftJoint], keypoints[rightJoint]]);
+
+        return result;
+      }, []);
+}
+
+function drawPoint(ctx, y, x, r, color) {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, 2 * Math.PI);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
+function toTuple({y, x}) {
+  return [y, x];
+}
+
+function drawSegment([ay, ax], [by, bx], color, scale, ctx) {
+  ctx.beginPath();
+  ctx.moveTo(ax * scale, ay * scale);
+  ctx.lineTo(bx * scale, by * scale);
+  ctx.lineWidth = 3;
+  ctx.strokeStyle = color;
+  ctx.stroke();
+}
+
+function drawSkeleton(keypoints, minConfidence, scale = 1) {
+  const adjacentKeyPoints =
+      getAdjacentKeyPoints(keypoints.keypoints, minConfidence);
+
+  adjacentKeyPoints.forEach((keypoints) => {
+    drawSegment(
+        toTuple(keypoints[0].position), toTuple(keypoints[1].position), "#00FF00",
+        scale, ctx);
+  });
+}
